14cb3ca92b6ac0b27d83888b5e24365fb38a85d6
Vinicius Antunes <vinicius.mct17@gmail.com>
2024-06-03 20:31:31 -0300

    Refactoring and adding comments

--- ant_colony_optimization/all_v2.py
+++ ant_colony_optimization/all_v2.py
@@ -12,8 +12,8 @@ SNAKE_INITIAL_BODY = [[40, 0], [30, 0], [20, 0], [10, 0]]
 
 NUMBER_OF_FRUITS = 10
 
-ANT_COUNT = 100
-ITERATIONS = 100
+NUMBER_OF_ANTS = 1
+ITERATIONS = 1
 
 WINDOW_X = 500
 WINDOW_Y = 500
@@ -41,16 +41,19 @@ class Snake:
         self.direction = RIGHT
         self.change_to = self.direction
 
+    # Recebe o comando que a cobra recebe e armazena no atributo change_to
     def change_direction(self, key):
-        if key == 'UP':
+        if key == UP:
             self.change_to = UP
-        elif key == 'DOWN':
+        elif key == DOWN:
             self.change_to = DOWN
-        elif key == 'LEFT':
+        elif key == LEFT:
             self.change_to = LEFT
-        elif key == 'RIGHT':
+        elif key == RIGHT:
             self.change_to = RIGHT
 
+    # Verifica se houve uma mudança de direcao e se a direcao atual da cobra nao eh contraria
+    # Armazena a nova direcao no atributo direction
     def update_direction(self):
         if self.change_to == UP and self.direction != DOWN:
             self.direction = UP
@@ -61,7 +64,8 @@ class Snake:
         elif self.change_to == RIGHT and self.direction != LEFT:
             self.direction = RIGHT
 
-    def move(self, fruits, score, last_capture_time):
+    # Atualiza posicao da cobra de acordo com a direcao recebida
+    def update_position(self):
         if self.direction == UP:
             self.position[1] -= GRID_SIZE
         elif self.direction == DOWN:
@@ -71,24 +75,51 @@ class Snake:
         elif self.direction == RIGHT:
             self.position[0] += GRID_SIZE
 
+    # Move a cobra e retorna o score
+    def move(self, fruits, score, last_eat_time):
+        self.update_position()
         self.body.insert(0, list(self.position))
+        
+        score, fruit_eaten = self.check_if_eat_fruit(fruits, score, last_eat_time)
 
+        # Marca o tempo demorado para comer a fruta
+        if fruit_eaten:
+            self.last_eat_time = time.time()
+        else:
+            self.body.pop()
+
+        return score, fruit_eaten
+
+    # Checa se a cobra comeu uma fruta
+    def check_if_eat_fruit(self, fruits, score, last_eat_time):
         for fruit in fruits:
             if self.position == fruit.position:
                 fruit.spawn = False
                 fruits.remove(fruit)
-                elapsed_time = time.time() - last_capture_time
-                elapsed_time = elapsed_time if elapsed_time < 5 else 5
-                score_increase = 50 + (50 - 10 * elapsed_time)
-                score += max(score_increase, 0)
+
+                score = self.calculate_score(score, last_eat_time)
+
                 return score, True
-        else:
-            self.body.pop()
 
         return score, False
 
+    # Calcula a pontuacao do jogador
+    def calculate_score(self, score, last_eat_time):
+        # Armazena o tempo que a cobra gastou entre a ultima fruta e a atual
+        elapsed_time = time.time() - last_eat_time
+        # Se o tempo for maior que 5, armazena apenas como 5 para o score nao mudar
+        elapsed_time = elapsed_time if elapsed_time < 5 else 5
+
+        # Calculo da pontuacao: Cada fruta comida vale 50
+        # Tempo demorado para comer - pontuacao: (0s: 50, 1s: 40,..., >5s: 0)
+        score_increase = 50 + (50 - 10 * elapsed_time)
+        score += max(score_increase, 0)
+
+        return score
+
 class Fruit:
     def __init__(self):
+        # Cria a fruta em uma posicao aleatoria da tela criada
         self.position = [
             random.randrange(1, (WINDOW_X // GRID_SIZE)) * GRID_SIZE, 
             random.randrange(1, (WINDOW_Y // GRID_SIZE)) * GRID_SIZE
@@ -98,7 +129,7 @@ class Fruit:
 class Game:
     def __init__(self):
         pygame.init()
-        pygame.display.set_caption('Snake')
+        pygame.display.set_caption('Ant Colony Optimazation - Snake')
 
         self.window = None
         self.clock = pygame.time.Clock()
@@ -107,8 +138,9 @@ class Game:
         self.fruits_coordinates = ()
         self.score = 0
         self.start_time = time.time()
-        self.last_capture_time = self.start_time  # Track the time of the last capture
+        self.last_eat_time = self.start_time
 
+    # Mostra a pontuacao do jogador e o tempo desde a ultima vez que comeu uma fruta
     def show_score_and_time(self):
         font = pygame.font.SysFont(FONT_STYLE, FONT_SIZE)
 
@@ -116,22 +148,22 @@ class Game:
         score_rect = score_surface.get_rect()
         self.window.blit(score_surface, score_rect)
 
-        # Calculate the elapsed time since the last capture
-        elapsed_time = time.time() - self.last_capture_time
+        # Mostra quanto tempo gastou desde a ultima vez que comeu a fruta
+        elapsed_time = time.time() - self.last_eat_time
         time_surface = font.render(f'Time: {elapsed_time:.1f}s', True, WHITE)
         time_rect = time_surface.get_rect()
         time_rect.topleft = (0, 30)
         self.window.blit(time_surface, time_rect)
 
-        # Show how many fruits user have captured
+        # Mostra quantas frutas a cobra ja comeu
         fruits_surface = font.render(f'Fruits: {10 - len(self.fruits)}', True, WHITE)
         fruits_rect = fruits_surface.get_rect()
         fruits_rect.topleft = (0, 60)
         self.window.blit(fruits_surface, fruits_rect)
 
+    # Finaliza o jogo e mostra a pontuacao final
     def game_over(self):
         end_time = time.time()
-        total_time = end_time - self.start_time
         font = pygame.font.SysFont(FONT_STYLE, 50)
         game_over_surface = font.render(f'Your Score is : {self.score:.0f}', True, RED)
         game_over_rect = game_over_surface.get_rect()
@@ -143,30 +175,39 @@ class Game:
         pygame.quit()
         quit()
 
-    def check_collision(self):
+    # Detecta colisoes com a parede ou consigo mesmo
+    def check_collision(self, detect_self_collision=False):
+        # Detecta se a cobra ultrapassou os limites do jogo
         if (
             self.snake.position[0] < 0 or self.snake.position[0] > WINDOW_X - GRID_SIZE or
             self.snake.position[1] < 0 or self.snake.position[1] > WINDOW_Y - GRID_SIZE
         ):
             self.game_over()
 
-        # for block in self.snake.body[1:]:
-        #     if self.snake.position == block:
-        #         self.game_over()
+        # Detecta se a cobra colidiu consigo mesma, se duas partes estao sobrepostas
+        if detect_self_collision:
+            for block in self.snake.body[1:]:
+                if self.snake.position == block:
+                    self.game_over()
 
-    def draw_elements(self):
+    # Desenha os elementos na tela: Jogo, Grade, Cobra, Frutas
+    def draw_elements(self, draw_path=False):
+        # Define um fundo preto
         self.window.fill(BLACK)
 
+        # Desenha uma grade cinza na tela com tamanho 10, formando um quadriculado
         for x in range(0, WINDOW_X, GRID_SIZE):
             pygame.draw.line(self.window, GRAY, (x, 0), (x, WINDOW_Y))
         for y in range(0, WINDOW_Y, GRID_SIZE):
             pygame.draw.line(self.window, GRAY, (0, y), (WINDOW_X, y))
 
+        # Desenha a cobra em verde
         for pos in self.snake.body:
             pygame.draw.rect(
                 self.window, GREEN, pygame.Rect(pos[0], pos[1], GRID_SIZE, GRID_SIZE)
             )
 
+        # Desenha as frutas em vermelho
         for fruit in self.fruits:
             pygame.draw.rect(
                 self.window, RED, pygame.Rect(
@@ -174,57 +215,82 @@ class Game:
                 )
             )
 
-        # path_points = self.discrete_path(self.snake.position, self.optimal_nodes[0])
+        # Desenha os caminhos que a cobra faz em amarelo
+        if draw_path:
+            path_points = self.discrete_path(self.snake.position, self.optimal_nodes[0])
 
-        # for point in path_points:
-        #     pygame.draw.rect(self.window, YELLOW, (point[0], point[1], GRID_SIZE, GRID_SIZE))
+            for point in path_points:
+                pygame.draw.rect(self.window, YELLOW, (point[0], point[1], GRID_SIZE, GRID_SIZE))
 
         self.show_score_and_time()
+
+        # Atualiza a tela do jogo
         pygame.display.update()
 
+    # Encontra a fruta a direita mais proxima da cobra para iniciar o caminho
     def find_closest_right_coordinate(self, coord, coord_list):
         x, y = coord
 
-        # Filtrar coordenadas à direita
+        # Filtra as coordenadas a direita
         right_coords = [c for c in coord_list if c[0] > x]
 
+        # Retorna None se nao houver coordenadas a direita
         if not right_coords:
-            return None  # Retorna None se não houver coordenadas à direita
+            return None
 
-        # Encontrar a coordenada à direita mais próxima
+        # Encontrar a coordenada a direita mais próxima
         closest_right_coord = min(right_coords, key=lambda c: c[0] - x)
 
         return closest_right_coord
 
+    # Acha um caminho discreto entre dois pontos através do algoritmo de Bresenham's
     def discrete_path(self, start, end):
+        # Lista que armazenara o caminho
         path = []
+        
+        # Coordenadas dos pontos inicial e final
         x1, y1 = start
         x2, y2 = end
+
+        # Diferencas em modulo nas coordenadas x e y
         dx = abs(x2 - x1)
         dy = abs(y2 - y1)
+
+        # Direcoes de incremento/decremento para x e y
         sx = 1 if x1 < x2 else -1
         sy = 1 if y1 < y2 else -1
+
+        # Valor inicial do erro
         err = dx - dy
 
         while True:
+            # Adiciona o ponto atual ao caminho
             path.append((x1, y1))
+
+            # Verifica se chegou no ponto final
             if x1 == x2 and y1 == y2:
                 break
+
+            # Atualiza o erro
             e2 = 2 * err
+
+            # Ajusta os erros e move os pontos nas direcoes x e y
             if e2 > -dy:
                 err -= dy
                 x1 += sx
+
             if e2 < dx:
                 err += dx
                 y1 += sy
 
-        # Adjust path points to be multiples of 10
+        # Ajusta os pontos para serem multiplos do tamanho da grade
         path = [
             (round(x / GRID_SIZE) * GRID_SIZE, round(y / GRID_SIZE) * GRID_SIZE) for x, y in path
         ]
 
         i = 0
 
+        # Adiciona pontos verticais ou horizontais a pontos conectados por diagonais
         while i < len(path) - 1:
             xx1, yy1 = path[i]
             xx2, yy2 = path[i + 1]
@@ -236,6 +302,7 @@ class Game:
 
         return path
 
+    # Converte a lista de pontos no caminho e converte em uma lista de direcoes
     def generate_commands(self, path_points):
         commands = []
 
@@ -244,29 +311,33 @@ class Game:
             x2, y2 = path_points[i + 1]
 
             if x1 < x2:
-                commands.append('RIGHT')
+                commands.append(RIGHT)
             elif x1 > x2:
-                commands.append('LEFT')
+                commands.append(LEFT)
             elif y1 < y2:
-                commands.append('DOWN')
+                commands.append(DOWN)
             elif y1 > y2:
-                commands.append('UP')
+                commands.append(UP)
  
         return commands
 
+    # Define as coordenadas das frutas e armazena em uma tupla
     def set_fruits_coordinates(self):
         fruits_coordinates = [fruit.position for fruit in self.fruits]
         fruits_coordinates = tuple([tuple(fruit) for fruit in fruits_coordinates])
 
         self.fruits_coordinates = fruits_coordinates
 
+    # Aplica o algoritmo de otimizacao por colonia de formigas para encontrar o melhor caminho
     def ant_colony_optimization(self):
-        colony = AntColony(self.fruits_coordinates, ant_count=ANT_COUNT, iterations=ITERATIONS)
+        colony = AntColony(self.fruits_coordinates, ant_count=NUMBER_OF_ANTS, iterations=ITERATIONS)
         optimal_nodes = colony.get_path()
         optimal_nodes.pop()
 
+        # Armazena a lista contendo o melhor caminho encontrado entre os pontos
         self.optimal_nodes = optimal_nodes
 
+    # Define os parametros para plotar o melhor caminho encontrado pelo algoritmo ACO
     def plot_nodes(self, w=5, h=5):
         for x, y in self.fruits_coordinates:
             plt.plot(x, y, "g.", markersize=15)
@@ -274,7 +345,8 @@ class Game:
         plt.axis("off")
         fig = plt.gcf()
         fig.set_size_inches([w, h])
-    
+
+    # Plota o melhor caminho encontrado pelo algoritmo ACO em um gráfico
     def plot_result(self):
         plt.style.use("dark_background")
 
@@ -287,55 +359,86 @@ class Game:
             )
 
         plt.gca().invert_yaxis()
-        # plt.show()
         plt.show(block=False)
         plt.pause(15)
         plt.close()
 
+    # A partir da lista de direcoes que a cobra deve seguir, inicia o caminho
     def command_snake(self, keys):
         for key in keys:
+            # Termina a execucao se o usuario clicar no X ou apertar a tecla 'Esc'
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+
             self.snake.change_direction(key)
             self.snake.update_direction()
 
             previous_score = self.score
 
-            self.score, captured = self.snake.move(self.fruits, self.score, self.last_capture_time)
+            self.score, fruit_eaten = self.snake.move(self.fruits, self.score, self.last_eat_time)
 
-            if captured:
-                self.last_capture_time = time.time()
+            # se uma fruta for comida, armazena o tempo que demorou
+            if fruit_eaten:
+                self.last_eat_time = time.time()
 
+            # se nao tem mais frutas, o jogo acaba
             if not self.fruits:
                 self.game_over()
 
             self.check_collision()
             self.draw_elements()
+
+            # Atualiza os frames do jogo
             self.clock.tick(SNAKE_SPEED)
 
-    def run(self):
-        self.set_fruits_coordinates()
-        self.ant_colony_optimization()
-
-        # Plot the result in a separate thread
-        # plot_thread = threading.Thread(target=self.plot_result)
-        # plot_thread.start()
-
-        keys = []
-
+    # Define os comandos da cobra ate a primeira fruta no inicio do jogo
+    def commands_to_first_fruit(self, keys):
+        # Encontra a primeira fruta e armazena seu indice
         first_fruit = self.find_closest_right_coordinate(self.snake.position, self.optimal_nodes)
         first_fruit_index = self.optimal_nodes.index(first_fruit)
 
+        # Muda a lista do melhor caminho para que a fruta mais proxima a direita seja a primeira
         self.optimal_nodes.remove(first_fruit)
         self.optimal_nodes = [first_fruit] + self.optimal_nodes[first_fruit_index:] + self.optimal_nodes[:first_fruit_index]
 
+        # Encontra o caminho discreto entre a cobra e a primeira fruta
         path_points = self.discrete_path(self.snake.position, self.optimal_nodes[0])
 
+        # Converte o caminho encontrado em comandos
         keys.extend(self.generate_commands(path_points))
 
+        return keys
+
+    # Define os comandos da cobra da primeira fruta ate todas as outras
+    def commands_to_other_fruits(self, keys):
         for i in range(len(self.optimal_nodes) - 1):
             path_points = self.discrete_path(self.optimal_nodes[i], self.optimal_nodes[i + 1])
 
             keys.extend(self.generate_commands(path_points))
 
+        return keys
+
+    # Roda o jogo
+    def run(self, plot_results=False):
+        self.set_fruits_coordinates()
+        self.ant_colony_optimization()
+
+        if plot_results:
+            # Utiliza uma thread separada para plotar os resultados
+            plot_thread = threading.Thread(target=self.plot_result)
+            plot_thread.start()
+
+        # Lista que armazenara os caminhos que a cobra deve fazer no jogo
+        keys = []
+
+        keys = self.commands_to_first_fruit(keys)
+        keys = self.commands_to_other_fruits(keys)
+
+        # Define a tela do jogo e inicia os comandos para controlar a cobra
         self.window = pygame.display.set_mode((WINDOW_X, WINDOW_Y))
         self.command_snake(keys)
 
